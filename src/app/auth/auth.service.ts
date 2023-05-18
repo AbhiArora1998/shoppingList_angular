b03a5fe8e14e492b803397d665b859c4dcb530fe
aarora <aarora@unbc.ca>
2023-05-18 06:20:06 -0700

    Login_working

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService{
     // behaviorsubject for the ondemand model. it can give you the value to a previous value even  before even subscibing it
     // it takes initial value which we will pass null 
     user = new BehaviorSubject<UserModel>(null);
-
+    tokenExpirationTime:any
     constructor(private http:HttpClient, private router:Router) {
         
     }
@@ -53,13 +53,50 @@ export class AuthService{
     }
 
     private handleAuthentication(email: string, userId:string,token:string, expiresIn:number) {
-        const expireInTime = new Date(new Date().getTime() + expiresIn*1000)
-        const user = new UserModel(email, userId, token, expireInTime)
+        const expireInTimes = new Date(new Date().getTime() + expiresIn*1000)
+        const user = new UserModel(email, userId, token, expireInTimes)
         this.user.next(user);
+        this.autoLogout(expiresIn*1000)
+        localStorage.setItem("userData",JSON.stringify(user))
     }
 
     logout(){
         this.user.next(null);
         this.router.navigate(["/auth"])
+        localStorage.removeItem("userData")
+
+        if (this.tokenExpirationTime) {
+            clearTimeout(this.tokenExpirationTime)
+        }
+        this.tokenExpirationTime = null
     }
+
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token:string,
+            _tokenExpirationDate:string,
+            
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new UserModel(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
+        
+        if (loadedUser.token) {
+            this.user.next(loadedUser)
+            // this.autoLogout(expiresIn*1000)
+            const expirationDuratioss = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+            this.autoLogout(expirationDuratioss)
+        }
+    }
+
+    autoLogout(expirationDuration: number) {
+       this.tokenExpirationTime =  setTimeout(() => {
+            this.logout();
+        },expirationDuration)
+    }
+
 }
